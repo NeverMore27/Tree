5fa98e5b15771523374a18d2b792adfc7b43d8ef
NeverMore27 <uragan.uragan.59@mail.ru>
2017-04-03 16:48:46 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -6,18 +6,18 @@
 SCENARIO("null")
 {
 	Tree<int> a;
-	REQURE(a.tree_one()=NULL);
+	REQUIRE(a.tree_one()=NULL);
 }
 SCENARIO("add") 
 {
 	Tree<int> a;
 	bool b=a.add(5);
-	REQURE(b == 1);
+	REQUIRE(b == 1);
 }
 SCENARIO("find")
 {
 	Tree<int> a;
 	a.add(5);
 	bool b = a.find(5);
-	REQURE(b == 1);
+	REQUIRE(b == 1);
 }
