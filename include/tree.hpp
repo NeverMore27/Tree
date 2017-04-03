cdc41a805acfde29fe55c41030e099cc93e8d40f
NeverMore27 <uragan.uragan.59@mail.ru>
2017-04-03 18:38:49 +0300

    Update tree.hpp

--- include/tree.hpp
+++ include/tree.hpp
@@ -17,9 +17,9 @@ private:
 		Node *left;
 		Node *right;
 		Node() {};
-		Node(const T value) : value_(value) { };
-		~Node() {};
-		void show(ostream &out, const int level) const;
+		Node(const T value) : value_(value) { }
+		~Node() {}
+		void show(ostream &out, const int level = 0) const;
 	};
 
 	Node *tree, *null_tr;
@@ -29,89 +29,29 @@ private:
 		if (tr_->left)
 		{
 			null_tree(tr_->left);
-			tr_->left = nullptr;
+			tr_->left =nullptr;
 		}
 		if (tr_->right)
 		{
 			null_tree(tr_->right);
-			tr_->right = nullptr;
+			tr_->right =nullptr;
 		}
 		delete tr_;
 	}
-	Node * & find_(const T value)
-	{
-
-		Node *tr = tree;
-		while ((tr) && (tr->value_ != value))
-		{
-			if (value < tr->value_)
-				tr = tr->left;
-			else tr = tr->right;
-		}
-		if (!tr) return null_tr;
-		else return tr;
-	}
-	void add_node(Node *&add_n, T value)
-	{
-		add_n = new Node(value);
-		add_n->left = nullptr;
-		add_n->right = nullptr;
-
-	}
-	Node* add_(const T value, Node * tr = 0)
-	{
-		if (!tree)
-		{
-			tree = new Node(value);
-			tree->left = nullptr;
-			tree->right = nullptr;
-			return tree;
-		}
-		if (!tr) tr = tree;
-		if ((tr) && (tr->value_ != value))
-
-		{
-			if (value < tr->value_)
-
-			{
-				if (tr->left)
-					add_(value, tr->left);
-				else
-				{
-					add_node(tr->left, value);
-					return tr->left;
-				}
-			}
-
-			else
-			{
-				if (tr->right)
-					add_(value, tr->right);
-				else
-				{
-					add_node(tr->right, value);
-					return tr->right;
-				}
-			}
-		}
-		return 0;
-	}
+	
 public:
 	Tree()
 	{
 		tree = NULL;
 		null_tr = nullptr;
 	};
-
+	Node* tree_one() { return tree };
 	void file_tree(char* name);
-	Node* tree_one ()
-	{
-		return tree;
-	};
-	bool add(const T value);
+	void add(const T value);
 	bool find(const T value);
+	bool isEmpty() const { return tree== NULL; }
 	void print(ostream &out) const;
-	~Tree()
+	~Tree() 
 	{
 		null_tree(tree);
 		null_tree(null_tr);
