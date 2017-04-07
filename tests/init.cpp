9ca8527152e4f03325a28cbbf6177dec2cad025c
NeverMore27 <uragan.uragan.59@mail.ru>
2017-04-07 20:48:25 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -21,3 +21,16 @@ SCENARIO("find")
 	bool b = a.find(5);
 	REQUIRE(b == 1);
 }
+SCENARIO("file")
+{
+	Tree<int> a;
+	a.file_tree("Tr.txt");
+	bool b = a.find(3);
+	REQUIRE(b == 1);
+	b = a.find(2);
+	REQUIRE(b == 1);
+	b = a.find(1);
+	REQUIRE(b == 1);
+	b = a.find(5);
+	REQUIRE(b == 1);
+}
